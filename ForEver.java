4e912664c99906eb836578d36f663b2b2b17d199
RoswindaAnnas <127526123+RoswindaAnnas@users.noreply.github.com>
2023-03-26 22:12:29 +0800

    Add files via upload

---
+++ ForEver.java
@@ -0,0 +1,22 @@
+/* NIM      : 13020210268
+Nama        : Roswinda.A
+Hari/Tgl    : Kamis, 23 Maret 2023
+Waktu       : 00.00  AM */
+package tgs2;
+
+public class ForEver {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Program */
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true){
+            System.out.print ("Print satu baris ....\n");
+        }
+    
+    }
+    
+}
